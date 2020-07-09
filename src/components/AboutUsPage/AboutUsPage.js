e3da56c566940889a75474814ee3d666ad721558
Sergey Aronov <sergey@h-MacBook-Pro-sl-Sergey.local>
2020-07-09 17:38:53 +0300

    created about page

--- src/components/AboutUsPage/AboutUsPage.js
+++ src/components/AboutUsPage/AboutUsPage.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import TeamMember from './../TeamMember/TeamMember';
+
 
 const AboutUsPage = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>
-                Sergey Aronov <br />
-                Karin Gluzman
-            </p>
+        <div>       
+            <TeamMember
+                img = {"https://i.ibb.co/qF9TVz9/karin.jpg"}
+                name = {"Karin Gluzman"}
+                description = {"Results-oriented customer success manager in the tech industry who specializes in quality customer success, customer support, customer retention, productivity, and team management. Highly experienced in software-as-a-service customer success and dedicated to providing excellent customer service and making operational and procedural improvements. Offer excellent communication, interpersonal skills, and problem-solving skills."}
+                linkedinURL = {"https://www.linkedin.com/in/karin-gluzman/"}
+                emailURL = {"mailto:karingluz@gmail.com"}
+            />  
+            <TeamMember
+                img = {"https://i.ibb.co/nrZfKNn/sergey.png"}
+                name = {"Sergey Aronov"}
+                description = {"Software Engineering graduate with a special interest in web development. Solid web development knowledge using JavaScript, HTML, CSS, React, Redux, Python, Flask and Sklearn."}
+                linkedinURL = {"https://www.linkedin.com/in/sergey-aronov-61a93a16a/"}
+                emailURL = {"mailto:aronovsergeyy@gmail.com"}
+            />
         </div>
     );
 };
---
+++ src/components/TeamMember/TeamMember.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    img: {
+      marginLeft: "auto",
+      marginRight: "auto",
+      marginBottom: "1em",
+      marginTop: "0.5em",
+      width: theme.spacing(30),
+      height: 280,
+    },
+    root: {
+        display: 'flex',
+        marginBottom: theme.spacing(5),
+    },
+  }));
+
+export default function TeamMember(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+        <CardMedia
+            className={classes.img}
+            component="img"
+            image={props.img}
+            title="Contemplative Reptile"
+        />
+        <div>
+            <CardActionArea>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.name}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {props.description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <a
+                    href={props.linkedinURL} >
+                    <h3>
+                        LinkedIn
+                    </h3>
+                </a>  
+                <a
+                    href={props.emailURL} >
+                    <h3>
+                        Email
+                    </h3>
+                </a>  
+            </CardActions>
+        </div>
+    </Card>
+  );
+}
